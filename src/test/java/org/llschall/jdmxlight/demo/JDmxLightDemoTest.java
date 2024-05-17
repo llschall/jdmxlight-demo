466d8802a9f78aea36ae77988cf0ad13e6ac53b3
llschall <llschall@gmx.com>
2024-05-17 11:11:46 +0200

    [Build] Import test from jdmxlight repository

---
+++ src/test/java/org/llschall/jdmxlight/demo/JDmxLightDemoTest.java
@@ -0,0 +1,17 @@
+package org.llschall.jdmxlight.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.llschall.jdmxlight.demo.model.DemoModel;
+
+public class JDmxLightDemoTest {
+
+    @Test
+    public void testSetup() {
+
+        DemoModel model = new DemoModel();
+        String title = model.createTitle();
+        Assertions.assertFalse(title.isBlank());
+    }
+
+}
