6528b90441f0f3adef2d2f998aa0b4fc29963123
llschall <llschall@gmx.com>
2024-05-17 17:40:45 +0200

    [Build] Generate application files with the wrapper

--- app/src/main/java/org/llschall/jdmxlight/demo/App.java
+++ app/src/main/java/org/llschall/jdmxlight/demo/App.java
@@ -2,7 +2,7 @@ package org.llschall.jdmxlight.demo;
 
 import org.llschall.jdmxlight.demo.controller.DemoController;
 
-public class Demo {
+public class App {
 
     public static void main(String[] args) {
         new DemoController().start();
