de092a9246de0d79dfab9f204a5a0b1fbf18060f
llschall <llschall@gmx.com>
2024-05-17 20:10:00 +0200

    [View] Improve Slider

--- app/src/main/java/org/llschall/jdmxlight/demo/view/CenterPanel.java
+++ app/src/main/java/org/llschall/jdmxlight/demo/view/CenterPanel.java
@@ -1,10 +1,18 @@
 package org.llschall.jdmxlight.demo.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 class CenterPanel extends JPanel {
 
     CenterPanel() {
-        add(new DemoSlider());
+
+        setLayout(new GridLayout(1, 0));
+
+        add(new DemoSlider("Rotation"));
+        add(new DemoSlider("Inclinaison"));
+        add(new DemoSlider("-"));
+        add(new DemoSlider("Gobos"));
+        add(new DemoSlider("-"));
     }
 }
--- app/src/main/java/org/llschall/jdmxlight/demo/view/DemoSlider.java
+++ app/src/main/java/org/llschall/jdmxlight/demo/view/DemoSlider.java
@@ -5,14 +5,40 @@ import java.awt.*;
 
 class DemoSlider extends JPanel {
 
-    DemoSlider() {
+    DemoSlider(String name) {
 
-        JSlider slider = new JSlider();
+        JSlider slider = new JSlider(0, 255);
         slider.setOrientation(JSlider.NORTH);
-        slider.setMinimum(1);
-        slider.setMinimum(512);
+        slider.setValue(0);
+        slider.setPaintTicks(true);
+
+        JLabel valueLbl = new JLabel();
+        valueLbl.setText("0");
+
+        slider.addMouseWheelListener(e -> {
+            int rotation = e.getWheelRotation();
+            int value = slider.getValue() - rotation;
+            slider.setValue(value);
+        });
+
+        slider.addChangeListener(e -> {
+            valueLbl.setText("" + slider.getValue());
+        });
+
+        setBorder(BorderFactory.createRaisedBevelBorder());
+
+        JPanel southPnl = new JPanel();
+        JLabel nameLbl = new JLabel(name);
+
+        nameLbl.setFont(nameLbl.getFont().deriveFont(Font.BOLD, 20));
+
+        southPnl.setLayout(new GridLayout(0, 1));
+        southPnl.add(valueLbl);
+        southPnl.add(nameLbl);
 
         setLayout(new BorderLayout());
         add(slider, BorderLayout.CENTER);
+        add(southPnl, BorderLayout.SOUTH);
+
     }
 }
