4b0f5e8184f5b0df7898c4e6a8381655839c4929
llschall <llschall@gmx.com>
2025-03-17 13:06:17 +0100

    [View] Edit value with left click

--- app/src/main/java/org/llschall/jdmxlight/demo/view/RawPanel.java
+++ app/src/main/java/org/llschall/jdmxlight/demo/view/RawPanel.java
@@ -10,7 +10,7 @@ public class RawPanel extends JPanel {
 
     RawPanel(int channels, int rows) {
         JPanel centerPnl = new JPanel();
-        centerPnl.setLayout(new GridLayout(rows,0));
+        centerPnl.setLayout(new GridLayout(rows, 0));
         for (int i = 1; i <= channels; i++) {
             centerPnl.add(new Slider(i));
         }
@@ -18,7 +18,7 @@ public class RawPanel extends JPanel {
         add(centerPnl, BorderLayout.CENTER);
     }
 
-    static class Slider extends JPanel {
+    class Slider extends JPanel {
 
         int value;
 
@@ -39,20 +39,19 @@ public class RawPanel extends JPanel {
             addMouseWheelListener(e -> {
                 int rotation = e.getWheelRotation();
                 this.value -= rotation;
-                if(this.value < 0) this.value =0;
+                if (this.value < 0) this.value = 0;
                 if (this.value > 255) this.value = 255;
-                value.setText(""+ this.value);
+                value.setText("" + this.value);
             });
 
             Border border = BorderFactory.createLineBorder(Color.GRAY);
             setBorder(border);
 
-
             addMouseListener(new MouseListener() {
                 @Override
                 public void mouseEntered(MouseEvent e) {
                     setBorder(BorderFactory.createLineBorder(
-                            Color.BLUE, 4)
+                            Color.BLUE, 2)
                     );
                 }
 
@@ -62,20 +61,33 @@ public class RawPanel extends JPanel {
                 }
 
                 @Override
-                public void mouseClicked(MouseEvent e) {}
+                public void mouseClicked(MouseEvent e) {
+                    String str = JOptionPane.showInternalInputDialog(RawPanel.this, "");
+                    try {
+                        int i1 = Integer.parseInt(str);
+                        if (i1 >= 0 && i1 < 255) {
+                            Slider.this.value = i1;
+                            value.setText("" + Slider.this.value);
+                        }
+                    } catch (NumberFormatException ex) {
+                        ex.printStackTrace();
+                    }
+                }
 
                 @Override
-                public void mousePressed(MouseEvent e) {}
+                public void mousePressed(MouseEvent e) {
+                }
 
                 @Override
-                public void mouseReleased(MouseEvent e) {}
+                public void mouseReleased(MouseEvent e) {
+                }
             });
         }
 
         String buildLabel(int i) {
-            if(i > 99) return Integer.toString(i);
-            if(i > 9) return "0"+i;
-            return "00"+i;
+            if (i > 99) return Integer.toString(i);
+            if (i > 9) return "0" + i;
+            return "00" + i;
         }
 
     }
